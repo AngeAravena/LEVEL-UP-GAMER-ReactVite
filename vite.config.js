2c072122fa6d04f1321eeef7172a78232e6ef6ae
AngeAravena <ange.aravena@duocuc.cl>
2026-01-19 18:15:24 -0300

    creacion de pruebas unitarias

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.js',
+    environment: 'jsdom',
+    setupFiles: './src/setupTest.js',
   },
 })
